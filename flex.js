99491a3c5a6dee02ece4f6acd305302b39876ad7
Andrew Brandt <abrandt@makeschool.com>
2015-02-18 16:51:12 -0800

    shouldnt copy/paste from SO

--- flex.js
+++ flex.js
@@ -20,5 +20,4 @@ app.get('/v1/product/:upc', function(req, res) {
   res.send(req.params.upc)
 })
 
-var port_number = server.listen(process.env.PORT || 3000);
-app.listen(port_number);
+app.listen(process.env.PORT || 3000);
